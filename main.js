be6a9bdd6ccb36f4e938811a0c325e31e76249a5
SeolheeKim-developer <79366075+SeolheeKim-developer@users.noreply.github.com>
2022-10-28 01:05:40 -0400

    JS: project filtering

--- main.js
+++ main.js
@@ -46,15 +46,35 @@ document.addEventListener('scroll', () => {
     } else {
         arrowUp.classList.remove('visible');
     }
-})
+});
 
 //Handle click on the "arrow up" button
 arrowUp.addEventListener('click', () =>{
     scrollIntoView('#home');
-})
+});
 
-
-
+//Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) =>{
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null){
+        return;
+    } 
+    projectContainer.classList.add('anim-out');
+    setTimeout(() => {
+        projects.forEach((project) => {
+            console.log(project.dataset.type);
+            if(filter ==='*' || filter === project.dataset.type){
+                project.classList.remove('invisible');
+            } else {
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    }, 300);
+});
 
 
 
@@ -63,4 +83,4 @@ arrowUp.addEventListener('click', () =>{
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: 'smooth'});
-}
+};
