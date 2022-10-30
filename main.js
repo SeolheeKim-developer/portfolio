42efe6ef0ce2e6c1aa903357a20f749528fd8154
SeolheeKim-developer <79366075+SeolheeKim-developer@users.noreply.github.com>
2022-10-29 23:22:59 -0400

    Fix the error

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ document.addEventListener('scroll', ()=>{
     if(window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     }else{
-navbar.classList.remove('navbar--dark');
+    navbar.classList.remove('navbar--dark');
     }
 });
 
@@ -17,7 +17,6 @@ navbar.classList.remove('navbar--dark');
 //Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click',(event) =>{
-    
     const target = event.target;
     const link = target.dataset.link;
     if (link == null) {
@@ -58,6 +57,7 @@ document.addEventListener('scroll', () => {
 //Handle click on the "arrow up" button
 arrowUp.addEventListener('click', () =>{
     scrollIntoView('#home');
+
 });
 
 //Projects
@@ -94,7 +94,69 @@ workBtnContainer.addEventListener('click', (e) =>{
     }, 300);
 });
 
+
+
+// 1. get all section elements
+// 2. observe all section with IntersectionObserver
+// 3. make menu item active when it is in our view
+
+const sectionIds = [
+    '#home', 
+    '#about', 
+    '#skills', 
+    '#work', 
+    '#testimonials', 
+    '#contact'
+];
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map((id) => document.querySelector(`[data-link="${id}"]`));
+//console.log(sections);
+//console.log(navItems);
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: 'smooth'});
+    selectNavItem(navItems[sectionIds.indexOf(selector)]);
 };
+const observerOptions ={
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3,
+}
+
+const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting && entry.intersectionRatio > 0) {
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+            
+            //console.log(index, entry.target.id);
+            //스크롤링이 아래로 되어서 페이지가 올라옴
+            if(entry.boundingClientRect.y < 0) {
+                selectedNavIndex = index + 1;
+            }else {
+                selectedNavIndex = index - 1;
+            }
+        }
+    });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach((section) => observer.observe(section));
+
+window.addEventListener('wheel', () =>{
+    if(window.scrollY ===0) {
+        selectedNavIndex =0;
+    } else if (
+        Math.round(window.scrollY + window.innerHeight) >=
+
+    document.body.clientHeight) {
+        selectedNavIndex = navItems.length - 1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+});
